a939c593b63c99503d926de0aae50c10da8214c2
Vishal Jha <vjha93011@gmail.com>
2026-01-13 13:03:40 +0530

    refactor: comment out MasalaChai class and enhance type definitions for User and config

--- typescript_setup/src/interface.ts
+++ typescript_setup/src/interface.ts
@@ -17,10 +17,10 @@ interface TeaRecipe {
   milk: number;
 }
 
-class MasalaChai implements TeaRecipe {
-  water = 100;
-  milk = 50;
-}
+// class MasalaChai implements TeaRecipe {
+//   water = 100;
+//   milk = 50;
+// }
 
 //interface
 
@@ -32,3 +32,41 @@ class Chai implements Cupsize {
   size: "small" | "large" = "large";
 }
 
+//AND in TYPES
+//both the typos should be available inside cup:MasalaChai in the case of &
+type BaseChai = { teaLeaves: number };
+type Extra = { masala: number };
+
+type MasalaChai = BaseChai & Extra;
+
+const cup: MasalaChai = {
+  teaLeaves: 20,
+  masala: 3,
+};
+
+//optinal chain
+type User = {
+  username: string;
+  bio?: string | undefined;
+};
+
+const u1: User = {
+  username: "vishal",
+};
+
+const u2: User = {
+  username: "rahul",
+  bio: "iam a full stack developer",
+};
+
+type config = {
+  readonly appName: string;
+  version: number;
+};
+
+const cfg: config = {
+  appName: "Master",
+  version: 1.4,
+};
+
+// cfg.appName="chaidoe";  //can't assign again once assigned beacause it is readonly
